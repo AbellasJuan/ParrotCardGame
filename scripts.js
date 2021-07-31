a4dd81119bdfe1df5c608458e34ea05b5aaa17b6
AbellasJuan <juanvictoraa@gmail.com>
2021-07-31 13:14:42 -0300

    trabalho completo sem refatorar

--- scripts.js
+++ scripts.js
@@ -1,16 +1,16 @@
 let ePrimeiraCarta = true;
 let primeiraCarta, segundaCarta;
-let numeroDeCartas; 
+let numeroDeCartas;
 let acabouJogo = 0;
 let contadorDeCliques=0;
-
 const imagens = ["cartaFrente1", "cartaFrente1", "cartaFrente2", "cartaFrente2", "cartaFrente3", "cartaFrente3", "cartaFrente4", "cartaFrente4", "cartaFrente5", "cartaFrente5", "cartaFrente6", "cartaFrente6", "cartaFrente7", "cartaFrente7"];
-
 const imagensSelecionadas = [];
 
-
 function quantasCartasQuerJogar(){ 
+            
     numeroDeCartas = Number(prompt("Olá!! Com quantas cartas quer jogar? Max: 14 cartas"));  
+    
+    console.log(numeroDeCartas);
     ValidarNumeroDeCartas();
 }
 
@@ -18,16 +18,15 @@ quantasCartasQuerJogar();
 
 function ValidarNumeroDeCartas(){
     if ((!Number(numeroDeCartas)) || (14 < numeroDeCartas) || (numeroDeCartas < 4) || (numeroDeCartas % 2 !== 0)){
-        numeroDeCartas = prompt("Você deve escolher um número par entre 4 e 14");
-        ValidarNumeroDeCartas();
+        alert("Você deve escolher um número par entre 4 e 14");
+        quantasCartasQuerJogar();
+        numeroDeCartas=0; //ele recebe isso antes de entrar na funçao
     }
     darAsCartas();
 }
 
 function darAsCartas(){
-
     let container = document.querySelector(".container");
-    
     for (let i = 1; i <= numeroDeCartas; i++) {  
   container.innerHTML += `<div onclick="selecionarCarta(this)" id="cartaNumero${i}" class="carta cartaCostas"></div>`
 }
@@ -47,19 +46,11 @@ function embaralharArray(){
 
     imagensSelecionadas.sort(comparador);
 //sort é usada para ordenar de acordo com a funçao
-//sort é usada para ordenar de acordo com a funçao
 }
 
 function comparador() { 
 	return Math.random() - 0.5;
 }
-
-// function inserirClasseNasCartas(){
-//     for (let i = 1; i <= numeroDeCartas; i++) {
-//     const element = document.getElementById("cartaNumero" + i);
-//     element.classList.add(imagensSelecionadas[i-1]);
-//     }
-// }
 
 function selecionarCarta(carta){
     
@@ -67,12 +58,15 @@ function selecionarCarta(carta){
     nomeDaId = nomeDaId.replace('cartaNumero',' ');
     carta.classList.add(imagensSelecionadas[nomeDaId-1]);
     // carta.classList.add(imagensSelecionadas[nomeDaId-1]);
-        
+    
+    //DEVE ESPERAR AQUI
+
     manterViradaOuDesvirarCarta(carta);
 }
 
 function manterViradaOuDesvirarCarta(carta){
-    
+    setTimeout(function () {
+
     if (ePrimeiraCarta){
         contadorDeCliques++
         primeiraCarta = carta
@@ -92,17 +86,10 @@ function manterViradaOuDesvirarCarta(carta){
 
             if(numeroDeCartas/2 === acabouJogo){
                 alert("Você ganhou em " + contadorDeCliques + " jogadas!");
-            } 
-            
-
-        //primeiraCarta.remove(imagensSelecionadas[nomeDaId-1]
-        
+            }        
         }
         ePrimeiraCarta = true 
     }
+  }, 1000);
 }
 
-//FALTA FAZER
-//virar a carta ao selecionar (fazer delay)
-//tentar fazer a transiçao = bonus
-//pra nao fazer o jogo mandar o ALERT quando clicar em cima da mesma carta, fazer uma condiçao que a id da primeiraCarta !== id da segundaCarta (ou a div inteira)
