a4e2fec63c97fd77000270471ed0c45170ca4a25
AbellasJuan <juanvictoraa@gmail.com>
2021-08-02 01:06:48 -0300

    Bug de clicar na mesma carta resolvido

--- scripts.js
+++ scripts.js
@@ -6,13 +6,8 @@ let contadorDeCliques=0;
 const imagens = ["cartaFrente1", "cartaFrente1", "cartaFrente2", "cartaFrente2", "cartaFrente3", "cartaFrente3", "cartaFrente4", "cartaFrente4", "cartaFrente5", "cartaFrente5", "cartaFrente6", "cartaFrente6", "cartaFrente7", "cartaFrente7"];
 const imagensSelecionadas = [];
 
-
-
 function quantasCartasQuerJogar(){ 
-    
     numeroDeCartas = Number(prompt("Olá!! Com quantas cartas quer jogar? Max: 14 cartas"));  
-    
-    
     ValidarNumeroDeCartas();
 }
 
@@ -28,15 +23,14 @@ function ValidarNumeroDeCartas(){
 }
 
 function darAsCartas(){
-    let container = document.querySelector(".container");
+    const container = document.querySelector(".container");
     for (let i = 1; i <= numeroDeCartas; i++) {  
-  container.innerHTML += `<div onclick="selecionarCarta(this)" id="cartaNumero${i}" class="carta cartaCostas"></div>`
+  container.innerHTML += `<div onclick="validarClique(this)" id="cartaNumero${i}" class="carta cartaCostas"></div>`
 }
 pegarImagens();
 }
 
 function pegarImagens(){
-
     for (let i=0; i<numeroDeCartas; i++) {
         let pegarImagem = imagens[i];
         imagensSelecionadas.push(pegarImagem);
@@ -45,13 +39,18 @@ embaralharArray()
 }
 
 function embaralharArray(){
-
     imagensSelecionadas.sort(comparador);
 //sort é usada para ordenar de acordo com a funçao
 }
 
 function comparador() { 
 	return Math.random() - 0.5;
+}
+
+function validarClique(carta){
+    if (carta.classList.length !== 3){
+        selecionarCarta(carta);
+    }
 }
 
 function selecionarCarta(carta){
@@ -61,6 +60,18 @@ function selecionarCarta(carta){
     //DEVE ESPERAR AQUI
     manterViradaOuDesvirarCarta(carta);
 }
+//-------------------------------------
+
+
+//TIRAR DUVIDA COM NIVEA
+// function desativarOnClick(){
+//     if (ePrimeiraCarta === false){
+//     let desativarBotao = document.querySelector("onclick")
+//     desativarBotao.onclick = null;
+//     }else(ePrimeiraCarta === true){
+//     desativarBotao.onclick = selecionarCarta(this);
+//     }
+// }
 
 //funçao pra ser dividida em varias menores
 function manterViradaOuDesvirarCarta(carta){
@@ -74,15 +85,11 @@ function manterViradaOuDesvirarCarta(carta){
         
         segundaCarta = carta
         if(primeiraCarta.classList[2] !== segundaCarta.classList[2]){
-            
-            
             segundaCarta.classList = "carta cartaCostas";
             primeiraCarta.classList = "carta cartaCostas";
         
         } else {
-
             acabouJogo++
-
             if(numeroDeCartas/2 === acabouJogo){
                 alert("Você ganhou em " + contadorDeCliques + " jogadas!");
                 
